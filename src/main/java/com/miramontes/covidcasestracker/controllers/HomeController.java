0896521b5a289294a9453ea5991b871ddcb557ba
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-06 18:47:23 -0600

    Passing some test data from the controller into the view

--- src/main/java/com/miramontes/covidcasestracker/controllers/HomeController.java
+++ src/main/java/com/miramontes/covidcasestracker/controllers/HomeController.java
@@ -1,13 +1,16 @@
 package com.miramontes.covidcasestracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("testname", "testvalue");
+
         // This looks up for this html file at templates folder.
         return "home";
     }
