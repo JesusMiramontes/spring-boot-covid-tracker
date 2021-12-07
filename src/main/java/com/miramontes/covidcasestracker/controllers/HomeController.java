8f16f365c5a09b1dfb5952f3255adab9ec9e01cf
JesusMiramontes <4751856+JesusMiramontes@users.noreply.github.com>
2021-12-06 19:39:10 -0600

    Included bootstrap

--- src/main/java/com/miramontes/covidcasestracker/controllers/HomeController.java
+++ src/main/java/com/miramontes/covidcasestracker/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package com.miramontes.covidcasestracker.controllers;
 
+import com.miramontes.covidcasestracker.models.LocationStat;
 import com.miramontes.covidcasestracker.services.CovidDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model){
-        model.addAttribute("locationStats", covidDataService.getCurrentStats());
+        List<LocationStat> currentStats = covidDataService.getCurrentStats();
+        int totalReportedCases = currentStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", currentStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
 
         // This looks up for this html file at templates folder.
         return "home";
